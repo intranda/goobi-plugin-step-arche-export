97a27a2c167c6e2ec8b36e719bbbe3a9531087bf
Robert Sehr <robert.sehr@intranda.com>
2025-11-18 10:22:40 +0100

    added option to switch between stage and prod ingests

--- module-lib/src/main/java/org/goobi/api/ArcheConfiguration.java
+++ module-lib/src/main/java/org/goobi/api/ArcheConfiguration.java
@@ -9,9 +9,14 @@ import lombok.Getter;
 @Getter
 public class ArcheConfiguration {
 
-    private String archeUserName;
-    private String archePassword;
-    private String archeApiUrl;
+    private String archeUserNameProd;
+    private String archePasswordProd;
+    private String archeApiUrlProd;
+
+    private String archeUserNameTest;
+    private String archePasswordTest;
+    private String archeApiUrlTest;
+
     private String archeUrlPropertyName;
     private String placeholderImage;
 
@@ -21,7 +26,8 @@ public class ArcheConfiguration {
     private String identifierPrefix = "https://id.acdh.oeaw.ac.at/";
     private String archeNamespace = "https://vocabs.acdh.oeaw.ac.at/schema#";
 
-    private boolean enableArcheIngest = false;
+    private boolean enableArcheIngestProd = false;
+    private boolean enableArcheIngestTest = false;
 
     private XMLConfiguration config;
 
@@ -29,11 +35,15 @@ public class ArcheConfiguration {
         config = ConfigPlugins.getPluginConfig(configurationFile);
         config.setExpressionEngine(new XPathExpressionEngine());
 
-        archeUserName = config.getString("/api/archeUserName");
-        archePassword = config.getString("/api/archePassword");
-        archeApiUrl = config.getString("/api/archeApiUrl");
+        archeUserNameProd = config.getString("/api[@type='prod']/archeUserName");
+        archePasswordProd = config.getString("/api[@type='prod']/archePassword");
+        archeApiUrlProd = config.getString("/api[@type='prod']/archeApiUrl");
+        enableArcheIngestProd = config.getBoolean("/api[@type='prod']/@enabled", false);
 
-        enableArcheIngest = config.getBoolean("/api/@enabled", false);
+        archeUserNameTest = config.getString("/api[@type!='prod']/archeUserName");
+        archePasswordTest = config.getString("/api[@type!='prod']/archePassword");
+        archeApiUrlTest = config.getString("/api[@type!='prod']/archeApiUrl");
+        enableArcheIngestTest = config.getBoolean("/api[@type!='prod']/@enabled", false);
 
         archeUrlPropertyName = config.getString("/project/archeUrlPropertyName");
         placeholderImage = config.getString("/project/placeholderImage");
@@ -41,6 +51,21 @@ public class ArcheConfiguration {
         viewerUrl = config.getString("/viewerUrl");
 
         exportFolder = config.getString("/exportFolder");
+    }
 
+    public boolean isEnableArcheIngest(boolean prod) {
+        return prod ? enableArcheIngestProd : enableArcheIngestTest;
+    }
+
+    public String getArcheApiUrl(boolean prod) {
+        return prod ? archeApiUrlProd : archeApiUrlTest;
+    }
+
+    public String getArcheUserName(boolean prod) {
+        return prod ? archeUserNameProd : archeUserNameTest;
+    }
+
+    public String getArchePassword(boolean prod) {
+        return prod ? archePasswordProd : archePasswordTest;
     }
 }
